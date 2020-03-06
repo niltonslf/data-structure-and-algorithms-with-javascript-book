596e9d0e213df0e288f2a8355c16e52d50ff3497
Nilton Lopes <nilton.lopes@madeiramadeira.com.br>
2020-03-05 23:01:30 -0300

    Complexidade O(1)

---
+++ src/js/complexity-of-algorithms/O1.js
@@ -0,0 +1,11 @@
+/**
+ * Um algoritmo com complexidade O(1) é um algoritmo em que sua complexidade é constante, ou seja, não importa qual seja 
+ * a entrada ele sempre leverá o mesmo tempo para ser executado.
+ */
+
+ function increment(num) {
+   return ++ num
+ }
+
+
+ console.log(increment(2));
